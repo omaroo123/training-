b936345ebe6f5fe8d576f7bc067804bcbd1b3735
omaroo123 <omarwaelwaheed@gmail.com>
2025-06-13 22:06:18 +0000

    e

---
+++ basic_training.py
@@ -0,0 +1,20 @@
+print("Hello, world!")
+
+
+name = input("What's your name? ")
+print(f"Nice to meet you, {name}!")
+
+
+color = input("What's your favorite color? ")
+print(f"{color} is a great choice! It’s such a vibrant color.")
+
+
+hobby = input("What's a hobby you enjoy? ")
+print(f"That sounds fun, {name}! I bet doing {hobby} helps you relax.")
+
+dream = input("whats your dream job ?")
+print(f"thats great hope you achieve your,{dream}")
+
+print("Thanks for chatting with me!")
+
+    
